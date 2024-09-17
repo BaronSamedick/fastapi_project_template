6b6ab5505e852f2232d7c88bb9912f3e4b6c8d37
ds.eruslanov <ds.eruslanov@tensor.ru>
2024-09-17 16:55:56 +0500

    * добавлен lifespan с закрытием соединения к бд при остановке приложения

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 import api
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.dispose()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api.router, prefix=settings.api.prefix)
