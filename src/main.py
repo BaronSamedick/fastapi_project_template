ad99a4802139c7f00aa0c5d2d9c21a7b05278588
ds.eruslanov <ds.eruslanov@tensor.ru>
2024-09-18 11:27:50 +0500

    * переопределен класс ответов по умолчанию на ORJSONResponse

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 import api
 from core.models import db_helper
@@ -12,5 +13,5 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
 app.include_router(api.router)
