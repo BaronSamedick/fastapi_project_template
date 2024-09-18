4b8c2e8d694e42f33e3c5e744e4c0ca633b7aae8
ds.eruslanov <ds.eruslanov@tensor.ru>
2024-09-18 10:17:07 +0500

    * добавлена генерация имени для ограничения unique с учетом всех полей

--- src/alembic/versions/2024_09_18_1014-0c583132ad89_update_users_table.py
+++ src/alembic/versions/2024_09_18_1014-0c583132ad89_update_users_table.py
@@ -1,8 +1,8 @@
 """update users table
 
-Revision ID: 477320b44ab7
+Revision ID: 0c583132ad89
 Revises: f213a7be30fa
-Create Date: 2024-09-17 17:54:29.460096
+Create Date: 2024-09-18 10:14:17.676173
 
 """
 
@@ -12,8 +12,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-# revision identifiers, used by Alembic.
-revision: str = "477320b44ab7"
+revision: str = "0c583132ad89"
 down_revision: Union[str, None] = "f213a7be30fa"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -22,10 +21,12 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.add_column("users", sa.Column("foo", sa.Integer(), nullable=False))
     op.add_column("users", sa.Column("bar", sa.Integer(), nullable=False))
-    op.create_unique_constraint(op.f("uq_users_foo"), "users", ["foo", "bar"])
+    op.create_unique_constraint(
+        op.f("uq_users_foo_bar"), "users", ["foo", "bar"]
+    )
 
 
 def downgrade() -> None:
-    op.drop_constraint(op.f("uq_users_foo"), "users", type_="unique")
+    op.drop_constraint(op.f("uq_users_foo_bar"), "users", type_="unique")
     op.drop_column("users", "bar")
     op.drop_column("users", "foo")
