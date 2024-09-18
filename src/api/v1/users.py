68be5911188392bcd9d78408a7dd9e0ef3b35dfc
ds.eruslanov <ds.eruslanov@tensor.ru>
2024-09-18 11:19:19 +0500

    * добавлен маршрут на создание пользователя

--- src/api/v1/users.py
+++ src/api/v1/users.py
@@ -1,10 +1,12 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.config import settings
 from core.models import db_helper
-from core.schemas.user import UserRead
-from crud.users import get_all_users
+from core.schemas.user import UserRead, UserCreate
+from crud.users import get_all_users, create_user_db
 
 router = APIRouter(
     prefix=settings.api.v1.users,
@@ -14,7 +16,16 @@ router = APIRouter(
 
 @router.get("/", response_model=list[UserRead])
 async def get_users(
-        session: AsyncSession = Depends(db_helper.session_getter)
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
 ):
     users = await get_all_users(session=session)
     return users
+
+
+@router.post("/", response_model=UserRead)
+async def create_user(
+        user_create: UserCreate,
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)]
+):
+    user = await create_user_db(session=session, user_create=user_create)
+    return user
